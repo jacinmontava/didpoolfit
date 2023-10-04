570340df8bb466bb30da944c54f57d5cc90d7a19
Jacin Montava <jmontava@gmail.com>
2023-10-04 17:15:50 +0200

    Initial Commit

---
+++ didpoolfit/didpoolfitApp.swift
@@ -0,0 +1,17 @@
+//
+//  didpoolfitApp.swift
+//  didpoolfit
+//
+//  Created by Jacinto Montava on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct didpoolfitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
